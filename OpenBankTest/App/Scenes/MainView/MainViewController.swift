1b85d3178f17e970e6fdd09743b3b2445ee00fe2
Emanuel Martinez <me@manumartinez.es>
2022-02-01 14:49:20 +0100

    Remove storyboard reference

---
+++ OpenBankTest/App/Scenes/MainView/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  OpenBankTest
+//
+//  Created by Manu Martinez on 1/2/22.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+
+	// MARK: - Dependencies
+
+
+	// MARK: - Inits
+
+
+}
