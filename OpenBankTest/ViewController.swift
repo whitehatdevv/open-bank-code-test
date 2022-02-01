2ab32b56fbdaac05ac9ee82123dce218916545d8
Emanuel Martinez <me@manumartinez.es>
2022-02-01 14:26:15 +0100

    Create base project structure

---
+++ OpenBankTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenBankTest
+//
+//  Created by Manu Martinez on 1/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
