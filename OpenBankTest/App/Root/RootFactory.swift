89ee5b870bf617e812b7b55e43ecc099e3452a4b
Emanuel Martinez <me@manumartinez.es>
2022-02-03 12:32:13 +0100

    Documented factory

--- OpenBankTest/App/Root/RootFactory.swift
+++ OpenBankTest/App/Root/RootFactory.swift
@@ -8,8 +8,14 @@
 import Foundation
 import Networking
 
+// MARK: - Abstract factory used for instance all the scenes
 struct RootFactory {
 
+	/// Create the main scene using `Dependency Injection` to set all the properties
+	/// - Parameters:
+	///  - client: APIClient used for the Network Requests
+	///  - wireframe: Responsible of navigation flow
+	/// - Returns: `MainViewController` with all the needed property configured
 	func createMainScene(_ client: APIClient, wireframe: MainViewWireframe) -> MainViewController {
 		let scene = MainViewController()
 		let provider = MainViewDataProvider(client)
