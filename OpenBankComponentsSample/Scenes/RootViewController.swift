8323680c14ab1f2137e9be842f98b2470e806493
Emanuel Martinez <me@manumartinez.es>
2022-02-04 12:10:52 +0100

    Create sample project and view controller

--- OpenBankComponentsSample/Scenes/RootViewController.swift
+++ OpenBankComponentsSample/Scenes/RootViewController.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import UIKit
+
+class RootViewController: UIViewController {
+
+	// MARK: - Properties
+	let vc: [(vc: UIViewController, name: String)] = []
+
+	// MARK: - Inits
+	init() {
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+}
