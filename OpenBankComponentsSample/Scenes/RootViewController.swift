bb187ed4f14f95efc98ce439ae3db3009b95c637
Emanuel Martinez <me@manumartinez.es>
2022-02-04 13:55:29 +0100

    Define the sample root view controller

--- OpenBankComponentsSample/Scenes/RootViewController.swift
+++ OpenBankComponentsSample/Scenes/RootViewController.swift
@@ -7,11 +7,15 @@
 
 import Foundation
 import UIKit
+import OpenBankComponents
 
 class RootViewController: UIViewController {
 
 	// MARK: - Properties
-	let vc: [(vc: UIViewController, name: String)] = []
+	private let tableView = UITableView(frame: .zero, style: .plain)
+	private let vc: [(vc: UIViewController, name: String)] = [
+		(vc: BubblesViewController(), name: "Bubbles")
+	]
 
 	// MARK: - Inits
 	init() {
@@ -22,4 +26,53 @@ class RootViewController: UIViewController {
 		fatalError("init(coder:) has not been implemented")
 	}
 
+	// MARK: - View Lifecycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setupViews()
+	}
+
 }
+
+// MARK: - Configure the view
+extension RootViewController {
+
+	func setupViews() {
+		navigationItem.title = "Open Bank Sample Project"
+
+		tableView.dataSource = self
+		tableView.delegate = self
+		view.fill(tableView)
+	}
+
+}
+
+// MARK: - UITableViewDataSource's methods
+extension RootViewController: UITableViewDataSource {
+
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return vc.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+		cell.textLabel?.text = vc[indexPath.row].name
+		return cell
+	}
+
+}
+
+// MARK: - UITableViewDelegate's methods
+extension RootViewController: UITableViewDelegate {
+
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let selected = vc[indexPath.row].vc
+		navigationController?.pushViewController(selected, animated: true)
+	}
+
+}
+
---
+++ OpenBankComponentsSample/Scenes/SampleScenes/BubblesViewController.swift
@@ -0,0 +1,28 @@
+//
+//  BubblesViewController.swift
+//  OpenBankComponentsSample
+//
+//  Created by Manu Martinez on 4/2/22.
+//
+
+import Foundation
+import UIKit
+
+class BubblesViewController: UIViewController {
+
+	// MARK: - Inits
+	init() {
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: - View Lifecycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+
+}
